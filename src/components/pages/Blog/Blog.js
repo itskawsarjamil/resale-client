7ba226388c29696f8df827fd4649496c86d71543
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2023-02-04 20:40:12 +0600

    some pages added

---
+++ src/components/pages/Blog/Blog.js
@@ -0,0 +1,126 @@
+import { useState } from "react";
+import useTitle from "../../hooks/useTitle";
+
+const Item = ({ title, children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="border rounded shadow-sm">
+            <button
+                type="button"
+                aria-label="Open item"
+                title="Open item"
+                className="flex items-center justify-between w-full p-4 focus:outline-none"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <p className="text-lg font-medium">{title}</p>
+                <div className="flex items-center justify-center w-8 h-8 border rounded-full">
+                    <svg
+                        viewBox="0 0 24 24"
+                        className={`w-3 text-gray-600 transition-transform duration-200 ${isOpen ? 'transform rotate-180' : ''
+                            }`}
+                    >
+                        <polyline
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeMiterlimit="10"
+                            points="2,7 12,17 22,7"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
+                </div>
+            </button>
+            {isOpen && (
+                <div className="p-4 pt-0">
+                    <p className="text-gray-700">{children}</p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+const Blog = () => {
+    useTitle("Blog");
+    return (
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
+                <div className="flex flex-col mb-16 sm:text-center">
+                    <a href="/" className="mb-6 sm:mx-auto">
+                        <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
+                            <svg
+                                className="w-10 h-10 text-deep-purple-accent-400"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                    </a>
+                    {/* <div className="max-w-xl md:mx-auto sm:text-center lg:max-w-2xl">
+                        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                            <span className="relative inline-block">
+                                <svg
+                                    viewBox="0 0 52 24"
+                                    fill="currentColor"
+                                    className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                                >
+                                    <defs>
+                                        <pattern
+                                            id="ec5d8ef5-b853-4714-b94f-df28ec98eeb7"
+                                            x="0"
+                                            y="0"
+                                            width=".135"
+                                            height=".30"
+                                        >
+                                            <circle cx="1" cy="1" r=".7" />
+                                        </pattern>
+                                    </defs>
+                                    <rect
+                                        fill="url(#ec5d8ef5-b853-4714-b94f-df28ec98eeb7)"
+                                        width="52"
+                                        height="24"
+                                    />
+                                </svg>
+                                <span className="relative">The</span>
+                            </span>{' '}
+                            quick, brown fox jumps over a lazy dog
+                        </h2>
+                        <p className="text-base text-gray-700 md:text-lg">
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                            accusantium doloremque rem aperiam, eaque ipsa quae.
+                        </p>
+                    </div> */}
+                </div>
+                <div className="space-y-4">
+                    <Item title="Difference between SQL and NoSQL">
+                        SQL databases are primarily called as Relational Databases (RDBMS); whereas NoSQL database are primarily called as non-relational or distributed database.SQL databases are table-based on the other hand NoSQL databases are either key-value pairs, document-based, graph databases or wide-column stores. This makes relational SQL databases a better option for applications that require multi-row transactions such as an accounting system or for legacy systems that were built for a relational structure.
+                    </Item>
+                    <Item title="What is JWT, and how does it work?">
+                        JSON Web Token (JWT) is an open standard for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).
+                        Authentication server verifies the credentials and issues a jwt signed using either a secret salt or a private key. User's Client uses the JWT to access protected resources by passing the JWT in HTTP Authorization header. Resource server then verifies the authenticity of the token using the secret salt/ public key.
+                    </Item>
+                    <Item title="What is the difference between javascript and NodeJS?">
+                        Javascript is a Scripting language. It is mostly abbreviated as JS. It can be said that Javascript is the updated version of the ECMA script. Javascript is a high-level programming language that uses the concept of Oops but it is based on prototype inheritance.
+
+
+                        NodeJS is a cross-platform and opensource Javascript runtime environment that allows the javascript to be run on the server-side. Nodejs allows Javascript code to run outside the browser. Nodejs comes with a lot of modules and mostly used in web development.
+                    </Item>
+                    <Item title="How does NodeJS handle multiple requests at the same time?">
+                        NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them. EventLoop is the listener for the EventQueue.
+                    </Item>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default Blog;
--- src/components/routes/Routes/Routes.js
+++ src/components/routes/Routes/Routes.js
@@ -3,20 +3,30 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../../layout/Main/Main';
 import ErrorPage from '../../pages/Shared/ErrorPage/ErrorPage';
 import Home from '../../pages/Home/Home/Home';
+import About from '../../pages/About/About';
+import Blog from '../../pages/Blog/Blog';
 
-const routes=createBrowserRouter([
+const routes = createBrowserRouter([
     {
-        path:'/',
-        element:<Main></Main>,
-        errorElement:<ErrorPage></ErrorPage>,
-        children:[
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
             {
-                path:'/',
-                element:<Home></Home>,
+                path: '/',
+                element: <Home></Home>,
             },
             {
-                path:'/home',
-                element:<Home></Home>,
+                path: '/home',
+                element: <Home></Home>,
+            },
+            {
+                path: '/about',
+                element: <About></About>,
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>,
             },
         ]
     }
