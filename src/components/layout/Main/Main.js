a0f462af846391241a7882cd4585ed8ae2114026
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2023-02-04 20:15:29 +0600

    header implement

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Nav from '../../pages/Shared/Nav/Nav';
 import { Outlet } from 'react-router-dom';
 import Footer from '../../pages/Shared/Footer/Footer';
+import Header from '../../pages/Shared/Header/Header';
 
 const Main = () => {
     return (
         <div>
-            <Nav></Nav>
+            <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
